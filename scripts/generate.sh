f945957f305a8d561cae7d577aa4a5fe1d01fd84
MarcoChit0 <marcoantonio.c.s.2000@gmail.com>
2025-07-16 11:03:06 -0300

    results from gemma pddl

---
+++ scripts/generate.sh
@@ -0,0 +1,14 @@
+number_of_problems=20
+number_of_samples=1
+prompt_type="pddl"
+task_type="outofdistribution"
+domains=("barman" "blocksworld" "childsnack" "depots" "driverlog" "grippers" "logistics" "satellite")
+for d in "${domains[@]}"; do
+    echo "----------------------------------------"
+    echo "Generating problems for domain: $d"
+    command="python src/learning_to_plan/main.py --generate -c src/configs/generate/gemini.json -n ${number_of_problems} -d ${d} --task_type=${task_type} --prompt_type=${prompt_type} -s ${number_of_samples}"
+    echo "Running command: $command"
+    eval $command
+    echo "Finished generating problems for domain: $d"
+    echo "----------------------------------------"
+done
